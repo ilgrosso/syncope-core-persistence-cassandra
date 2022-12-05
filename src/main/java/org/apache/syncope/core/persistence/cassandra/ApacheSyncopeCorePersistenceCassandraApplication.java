e48e5a19a86d901eaa44aaed26babcf4a0b18b3a
Francesco Chicchiriccò <ilgrosso@apache.org>
2022-12-05 11:34:42 +0100

    Initial commit

---
+++ src/main/java/org/apache/syncope/core/persistence/cassandra/ApacheSyncopeCorePersistenceCassandraApplication.java
@@ -0,0 +1,12 @@
+package org.apache.syncope.core.persistence.cassandra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApacheSyncopeCorePersistenceCassandraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApacheSyncopeCorePersistenceCassandraApplication.class, args);
+    }
+}
---
+++ src/test/java/org/apache/syncope/core/persistence/cassandra/ApacheSyncopeCorePersistenceCassandraApplicationTests.java
@@ -0,0 +1,12 @@
+package org.apache.syncope.core.persistence.cassandra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = PersistenceTestContext.class)
+class ApacheSyncopeCorePersistenceCassandraApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
